d600819c07867e0cc125f0490ab8c672119b89d0
George Mylonas <geomylonas@gmail.com>
2018-01-14 06:33:38 +0200

    added findByDescription query methods

--- src/main/java/georgemylonas/recipe/Repositories/CategoryRepository.java
+++ src/main/java/georgemylonas/recipe/Repositories/CategoryRepository.java
@@ -1,7 +1,11 @@
 package georgemylonas.recipe.Repositories;
 
 import georgemylonas.recipe.domain.Category;
+import org.springframework.context.annotation.Description;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription(String description);
 }
--- src/main/java/georgemylonas/recipe/Repositories/UnitOfMeasureRepository.java
+++ src/main/java/georgemylonas/recipe/Repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,8 @@ package georgemylonas.recipe.Repositories;
 import georgemylonas.recipe.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
