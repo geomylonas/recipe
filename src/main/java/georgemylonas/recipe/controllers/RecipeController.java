8726236c32f508ef52cf5e5bd42c8c77ffdab82c
George Mylonas <geomylonas@gmail.com>
2018-01-25 11:01:53 +0200

    added recipe form and updated recipe controller

--- src/main/java/georgemylonas/recipe/controllers/RecipeController.java
+++ src/main/java/georgemylonas/recipe/controllers/RecipeController.java
@@ -1,10 +1,13 @@
 package georgemylonas.recipe.controllers;
 
 import georgemylonas.recipe.Services.RecipeService;
+import georgemylonas.recipe.commands.RecipeCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -22,9 +25,18 @@ public class RecipeController {
 
 
     }
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+        return "recipe/recipeform";
+    }
+    @PostMapping
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
-
-
+        return "redirect:/recipe/show/" + savedCommand.getId();
+    }
 
 }
 
